fde57548fc30360c84baabc755130da46bf26747
Michael Dotson <thedotson@icloud.com>
2020-11-30 19:01:52 -0600

    ch 10

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,8 +2,10 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
-import { EventList } from "./game/EventList.js"
-import { EventProvider } from "./game/EventProvider.js"
+import { EventList } from "./event/EventList.js"
+import { EventProvider } from "./event/EventProvider.js"
+import { GameForm } from "./game/GameForm.js"
+import { EventForm } from "./event/EventForm.js"
 
 export const ApplicationViews = () => {
     return <>
@@ -12,6 +14,7 @@ export const ApplicationViews = () => {
             lineHeight: "1.75rem"
         }}>
             <GameProvider>
+                <Route exact path="/games/new" render={props => <GameForm {...props} />} />
                 <Route exact path="/">
                     <GameList />
                 </Route>
@@ -21,6 +24,9 @@ export const ApplicationViews = () => {
                 <Route exact path="/events">
                     <EventList />
                 </Route>
+                <GameProvider>
+                    <Route exact path="/events/new" render={props => <EventForm {...props} />} />
+                </GameProvider>
             </EventProvider>
         </main>
     </>
